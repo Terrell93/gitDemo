b67c24184eff84e93579133b951440e2f7c12c53
Terrell Ncube <teeyncube@gmail.com>
2026-03-10 10:16:01 +0200

    Add request model

---
+++ GitDemo/Application/Models/ElevatorRequest.cs
@@ -0,0 +1,8 @@
+namespace GitDemo.Application.Models;
+
+public class ElevatorRequest
+{
+    public int ElevatorNumber { get; set; }
+    public int MaxWeight { get; set; }
+    public string Message { get; set; }
+}
