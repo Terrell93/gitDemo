9fb4aa194bcf54713b317ebb7a0ee228036b4159
Terrell Ncube <teeyncube@gmail.com>
2026-03-10 10:07:45 +0200

    add model

---
+++ GitDemo/Application/Models/Elevator.cs
@@ -0,0 +1,13 @@
+namespace GitDemo.Application.Models;
+
+public class Elevator(int number)
+{
+    public int Number { get; set; } = number + 1;
+    public bool Open { get; set; } = false;
+    public int CurrentFloor { get; set; } = 1;
+    public int TargetFloor { get; set; }
+    public int MaxWeight { get; set; } = 600;
+    public int NumberOfPeople { get; set; } = 0;
+    public bool Busy { get; set; } = false;
+    public string StatusMessage { get; set; } = "";
+}
